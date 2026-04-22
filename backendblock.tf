b2ccd9199dc2b8cbb02712edd8d27a448c472ee1
udayreddy216-code <udayreddychalla216@gmail.com>
2026-04-22 17:08:33 +0530

    Delete backendblock.tf

--- backendblock.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateuday12345"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
