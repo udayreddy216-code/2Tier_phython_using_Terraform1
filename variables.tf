1ace0718e8d8e6949d0c558085b93fd5acc93b3e
udayreddy216-code <udayreddychalla216@gmail.com>
2026-04-22 14:58:17 +0530

    commit code

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  default = "East Asia"
+}
+
+variable "resource_group_name" {
+  default = "Uday_rg"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "Password123!"
+}
+
+variable "mysql_admin" {
+  default = "ordinary"
+}
+
+variable "mysql_password" {
+  default = "StrongPass#123"
+}
+
