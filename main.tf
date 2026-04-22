3c25c6d7243e333af41e12f1c53550a8a94b521f
udayreddy216-code <udayreddychalla216@gmail.com>
2026-04-22 17:09:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -143,23 +143,6 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "allow_vm" {
   ]
 }
 
-resource "azurerm_resource_group" "tfstate_rg" {
-  name     = "tfstate-rg"
-  location = "East Asia"
-}
 
-resource "azurerm_storage_account" "tfstate_sa" {
-  name                     = "tfstateuday12345"   # must be globally unique
-  resource_group_name      = azurerm_resource_group.tfstate_rg.name
-  location                 = azurerm_resource_group.tfstate_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate_container" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate_sa.name
-  container_access_type = "private"
-}
 
 
